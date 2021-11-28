4d2b26ebd850a89762d23be6cbf37351ef4e5b4e
tabeaDargatz <92884948+tabeaDargatz@users.noreply.github.com>
2021-11-28 15:16:52 +0100

    Handler works, except that the openLockScreen() function doesn't work from the handelr -.-

--- app/src/main/java/com/example/stopdrunktexts/MainActivity.java
+++ app/src/main/java/com/example/stopdrunktexts/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.usage.UsageStatsManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
@@ -16,32 +17,19 @@ import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-import static java.lang.Thread.sleep;
-
 public class MainActivity extends AppCompatActivity {
 
+    final Handler handler = new Handler(Looper.myLooper());
+    final int delay = 1000;
     boolean activated = false;
     Button btnMain;
     String currentlyRunningApp;
-    final Handler handler = new Handler();
-    final int delay = 1000;
     Intent intent;
-/*
-Shitty handler stuff that doesn't work:
-    Runnable runnable = new Runnable() {
-        public void run() {
-            currentlyRunningApp = getCurrentApp();
-            if (currentlyRunningApp.equals("com.whatsapp")) {
-                killHandler();
-                System.out.println("Whatsapp detected. Showing lockscreen...");
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-            handler.postDelayed(this, delay);
-        }
-    };
+    boolean test = false;
 
- */
+    public void testActivate(View view){
+        test = true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -52,7 +40,59 @@ Shitty handler stuff that doesn't work:
     }
 
 
-    //TODO: Ask for permissions on app start if not already granted, remove permission buttons or hide them in sidebar
+    Runnable runnable = new Runnable() {
+        public void run() {
+            currentlyRunningApp = getCurrentApp();
+            if (currentlyRunningApp.equals("com.whatsapp")) {
+                System.out.println("Whatsapp detected. Showing lockscreen...");
+                openLockScreen();
+                openLockScreen();
+                handler.removeCallbacks(runnable);
+            }
+            else{
+                handler.postDelayed(this, delay);
+            }
+
+        }
+    };
+
+
+    //Doesn't work if called from runnable -.-
+    public void openLockScreen(){
+        intent = new Intent(this, LockScreen.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+    }
+
+    //TODO: Somehow the text change is not displayed... maybe the activity doesn't update or some shit
+    // that's a problem for future me (Should work when I get the fucking handler to work... hopefully)
+    public void startListening(View view)
+    {
+        if(!activated)
+        {
+            //Sad tries to make this shit work with a service or a handler:
+            //startService(new Intent(this, CheckForAppsAndDisplayLock.class));
+            handler.postDelayed(runnable,delay);
+            activated = true;
+            btnMain.setText("Deactivate Lock");
+        }
+        else{
+            activated = false;
+            //stopService(new Intent(this, CheckForAppsAndDisplayLock.class));
+            btnMain.setText("Activate Lock");
+            openLockScreen();
+            handler.removeCallbacks(runnable);
+        }
+    }
+
+
+
+
+
+
+ //-------------------------------------------DONT TOUCH BELOW BECAUSE THESE WORK FINE FOR NOW-----------------------------------------------------------------------
+
+//TODO: Ask for permissions on app start if not already granted, remove permission buttons or hide them in sidebar
 
     //Permission for UsageStatsManager to work ( function: "getCurrentApp")
     public void givePermissions1(View view)
@@ -66,51 +106,6 @@ Shitty handler stuff that doesn't work:
     {
         startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION));
     }
-
-
-    public void startListening(View view)
-    {
-        if(!activated)
-        {
-            //Sad tries to make this shit work with a service or a handler:
-            //startService(new Intent(this, CheckForAppsAndDisplayLock.class));
-            //handler.postDelayed(runnable,delay);
-            activated = true;
-
-            //TODO: Somehow the text change is not displayed... maybe the activity doesn't update or some shit
-            // that's a problem for future me (Should work when I get the fucking handler to work... hopefully)
-            btnMain.setText("Deactivate Lock");
-            System.out.println("Lock activated.");
-            while (activated) {
-                currentlyRunningApp = getCurrentApp();
-                if (currentlyRunningApp.equals("com.whatsapp")) {
-                    System.out.println("Whatsapp detected. Showing Lockscreen...");
-                    intent = new Intent(this, LockScreen.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                    startActivity(intent);
-                    activated = false;
-                }
-
-                try {
-                    sleep(800);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        else{
-            activated = false;
-            //stopService(new Intent(this, CheckForAppsAndDisplayLock.class));
-            btnMain.setText("Activate Lock");
-            System.out.println("Lock deactivated.");
-        }
-    }
-/*
-    public void killHandler(){
-        handler.removeCallbacks(runnable);
-    }
-
- */
 
 
 
@@ -137,6 +132,12 @@ Shitty handler stuff that doesn't work:
             }
         }
         System.out.println(topPackageName);
-        return topPackageName;
+        if(test){
+          return "com.whatsapp";
+        }
+        else{
+            return topPackageName;
+        }
+
     }
 }
